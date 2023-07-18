5580f8519df05192f54140010eeb9a8a5d8f7368
Serhii-Prusak <serhii.prusak@gmail.com>
2023-07-18 17:03:20 +0200

    products.json created, updated logic for price calculations

--- main.py
+++ main.py
@@ -4,23 +4,11 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
+import json
 
 # Define the products and their URLs
-products = [
-    {'name': 'Arkham Horror: Dunwich - Investigators', 'url': 'https://www.amazon.de/dp/B09M4F9P5V'},
-    {'name': 'Arkham Horror: Dunwich - Campaign', 'url': 'https://www.amazon.de/dp/B09M4GQVH5'},
-    {'name': 'Arkham Horror: Carcosa - Investigators', 'url': 'https://www.amazon.de/dp/B09V1TLZD2'},
-    {'name': 'Arkham Horror: Carcosa - Capmaign', 'url': 'https://www.amazon.de/dp/B09V1V2MZW'},
-    {'name': 'The Cartographers', 'url': 'https://www.amazon.de/dp/B07ZQPY1TX'},
-    {'name': 'Codenames Duet', 'url': 'https://www.amazon.de/dp/B072J234ZF'},
-    {'name': 'Welcome To', 'url': 'https://www.amazon.de/dp/B09T3K5TZL'},
-    {'name': 'Pixel Tactics', 'url': 'https://www.amazon.de/dp/B015482LJU'},
-    {'name': 'Sagrada', 'url': 'https://www.amazon.de/dp/B07CLRY89M'},
-    {'name': 'Boss Monster', 'url': 'https://www.amazon.de/dp/B01BLNVW24'},
-    {'name': 'Splendor', 'url': 'https://www.amazon.de/dp/B00ORBVDNQ'},
-    {'name': 'Terra Nova', 'url': 'https://www.amazon.de/dp/B0B2TW65CK'},
-    {'name': 'Boss Monster - Big Box', 'url': 'https://www.amazon.de/dp/B09T3LCP1B'}
-]
+with open ('products.json') as f:   
+    products = json.load(f)
 
 # Configure Selenium options
 chrome_options = Options()
@@ -33,20 +21,19 @@ webdriver_service = Service('/usr/local/bin')
 new_prices = []
 
 df = pd.read_csv('prices.csv')
-df['OldPrice'] = df['NewPrice']
+df['Old_Price'] = df['New_Price']
 # print(df)
 product_list = list(df['Product'])
 # print(product_list)
 # Loop through each product
 i = 1
 for product in products:
-    # print(f"---------------{product['name']}------------------")
 
     # Create a new Selenium WebDriver instance
     driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
     # Load the product page
-    driver.get(product['url'])
+    driver.get(product["url"])
     
     html = driver.page_source
 
@@ -69,22 +56,40 @@ for product in products:
             # check if data if product is in the file or no
             if product['name'] in product_list:
                 df.iloc[df.index[df['Product'] == product['name']], 2] = new_price
+                # check if current price is minimum
                 if df.iloc[df.index[df['Product'] == product['name']], 4].sum() > new_price:
                     df.iloc[df.index[df['Product'] == product['name']], 4] = new_price
+                    df.iloc[df.index[df['Product'] == product['name']], 6] = 'Minimum is updated'
+                else:
+                    if df.iloc[df.index[df['Product'] == product['name']], 5].sum() == 0:
+                        df.iloc[df.index[df['Product'] == product['name']], 6] = 'It is known minimum, you can buy'
+                    else:
+                        df.iloc[df.index[df['Product'] == product['name']], 6] = '-'
             else:
-                dict_add = {'Product': product['name'], 'OldPrice': new_price, 'NewPrice': new_price, 'Difference': 0, 'Minimum': new_price}
-                df.loc[len(df)] = dict_add
+                df.loc[len(df)] = {
+                    'Product': product['name'], 
+                    'Old_Price': new_price, 
+                    'New_Price': new_price, 
+                    'Difference': 0, 
+                    'Minimum_Price': new_price, 
+                    'Difference_Minimun':0, 
+                    'Comment': 'New'
+                    }
             break
     i += 1
 
-    # Quit the WebDriver
     driver.quit()
 
+product_names = [p['name'] for p in products]
+for product in product_list:
+    if product not in product_names:
+        df.iloc[df.index[df['Product'] == product], 6] = 'Product was removed from tracking'
+    else:
+        df.iloc[df.index[df['Product'] == product], 6] = '-'
 # Update the difference betweeen New and Old prices in our file 
-df['Difference'] = df['NewPrice'] - df['OldPrice']
-
-# Show the result df
+df['Difference'] = df['New_Price'] - df['Old_Price']
+df['Difference_Minimum'] = df['New_Price'] - df['Minimum_Price']
+df.sort_values(['New_Price'], inplace=True, ignore_index=True, ascending=True)
 print(df)
 
-# Save df to csv
 df.to_csv('prices.csv', index=False)
